2aace9faf150f7bef888b956d5e384ee70eee71f
Mathias.Romankiewicz <Mathias.romankiewicz@gmail.com>
2023-09-08 11:14:13 +0200

    add PutMapping/DeleteMapping

--- backend/src/main/java/de/iav/backend/controller/TestBenchOperatorController.java
+++ backend/src/main/java/de/iav/backend/controller/TestBenchOperatorController.java
@@ -1,7 +1,9 @@
 package de.iav.backend.controller;
 
+import de.iav.backend.exceptions.NoSuchTestBenchOperatorExeption;
 import de.iav.backend.model.TestBenchOperator;
 import de.iav.backend.service.TestBenchOperatorService;
+import jdk.incubator.vector.VectorOperators;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -25,5 +27,17 @@ public class TestBenchOperatorController {
     @ResponseStatus(HttpStatus.CREATED)
     public TestBenchOperator addTestBenchOperator(@RequestBody TestBenchOperator testBenchOperatorToAdd){
         return testBenchOperatorService.addTestBenchOperator(testBenchOperatorToAdd);
+    }
+
+    @PutMapping("/{operatorId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public TestBenchOperator updateTestBenchOperator(@PathVariable String operatorId, @RequestBody TestBenchOperator testBenchOperatorToUpdate) throws NoSuchTestBenchOperatorExeption {
+        return testBenchOperatorService.updateTestBenchOperatorById(operatorId, testBenchOperatorToUpdate);
+    }
+
+    @DeleteMapping("/{operatorId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTestBenchOperator(@PathVariable String operatorId){
+        testBenchOperatorService.deleteTestBenchOperator(operatorId);
     }
 }
