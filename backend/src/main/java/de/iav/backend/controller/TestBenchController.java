cfae43e2461453f9eb7497f46ade6cd5d0340725
Mathias.Romankiewicz <Mathias.romankiewicz@gmail.com>
2023-09-08 11:50:21 +0200

    add Put/DeleteMapping

--- backend/src/main/java/de/iav/backend/controller/TestBenchController.java
+++ backend/src/main/java/de/iav/backend/controller/TestBenchController.java
@@ -1,5 +1,8 @@
 package de.iav.backend.controller;
 
+import de.iav.backend.exceptions.NoSuchMetrologyException;
+import de.iav.backend.exceptions.NoSuchTestBenchException;
+import de.iav.backend.exceptions.NoSuchTestBenchOperatorExeption;
 import de.iav.backend.model.TestBench;
 import de.iav.backend.service.TestBenchService;
 import lombok.RequiredArgsConstructor;
@@ -19,14 +22,55 @@ public class TestBenchController {
     @GetMapping
     @ResponseStatus(HttpStatus.ACCEPTED)
     public List<TestBench> getAllTestBenches(){
-        return testBenchService.getAllTestBenches();
+        return testBenchService.listAllTestBenches();
     }
 
     @GetMapping("/{testBenchId}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public Optional<TestBench> getTestBenchById(@PathVariable String testBenchId){
-        return testBenchService.getTestBenchById(testBenchId);
+        return testBenchService.findTestBenchById(testBenchId);
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TestBench addTestBench(@RequestBody TestBench testBenchToAdd){
+        return testBenchService.addTestBench(testBenchToAdd);
+    }
+
+    @PutMapping("/{testBenchId}/maintenance")
+    @ResponseStatus(HttpStatus.CREATED)
+    public TestBench setTestBenchMaintenanceDate(@PathVariable String testBenchId, @RequestBody int date, int month, int year) throws NoSuchTestBenchException {
+        return testBenchService.setTestBenchMaintenanceDate(testBenchId, date, month, year);
+    }
+
+    @PutMapping("/{testBenchId}/calibration")
+    @ResponseStatus(HttpStatus.CREATED)
+    public TestBench setTestBenchCalibrationDate(@PathVariable String testBenchId, @RequestBody int date, int month, int year) throws NoSuchTestBenchException {
+        return testBenchService.setTestBenchCalibrationDate(testBenchId, date, month, year);
+    }
+
+    @PutMapping("/metrology/{testBenchId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addMetrologyToTestBench(@PathVariable String testBenchId, @RequestBody String metrologyId) throws NoSuchTestBenchException, NoSuchMetrologyException {
+        testBenchService.addMetrologyToTestBench(testBenchId, metrologyId);
+    }
+
+    @DeleteMapping("/metrology/{testBenchId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeMetrologyFromTestBench(@PathVariable String testBenchId, @RequestBody String metrologyId) throws NoSuchTestBenchException, NoSuchMetrologyException {
+        testBenchService.removeMetrologyFromTestBench(testBenchId, metrologyId);
+    }
+
+    @PutMapping("/operator/{testBenchId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addTestBenchOperatorToTestBench(@PathVariable String testBenchId, @RequestBody String operatorId) throws NoSuchTestBenchException, NoSuchTestBenchOperatorExeption {
+        testBenchService.addTestBenchOperatorToTestBench(testBenchId, operatorId);
+    }
+
+    @DeleteMapping("/operator/{testBenchId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeTestBechOperatorFromTestBench(@PathVariable String testBenchId, @RequestBody String operatorId) throws NoSuchTestBenchException, NoSuchTestBenchOperatorExeption {
+        testBenchService.removeTestBenchOperatorFromTestBench(testBenchId, operatorId);
+    }
 
 }
