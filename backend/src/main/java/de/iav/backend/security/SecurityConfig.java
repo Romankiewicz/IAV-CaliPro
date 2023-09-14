62623eb298730e73b2782ce0871942c79ecff4f2
Mathias.Romankiewicz <Mathias.romankiewicz@gmail.com>
2023-09-14 15:41:11 +0200

    add Security

---
+++ backend/src/main/java/de/iav/backend/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package de.iav.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .authorizeHttpRequests(customizer -> {
+                    customizer.requestMatchers(HttpMethod.GET, "").permitAll();
+                    customizer.requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll();
+                    customizer.anyRequest().permitAll();
+                })
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+}
--- backend/src/main/java/de/iav/backend/service/Argon2Service.java
+++ backend/src/main/java/de/iav/backend/service/Argon2Service.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class Argon2Service {
 
-    Argon2PasswordEncoder argon2PasswordEncoder = new Argon2PasswordEncoder();
+    Argon2PasswordEncoder argon2PasswordEncoder = Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
     public String encode(String password){
         return argon2PasswordEncoder.encode(password);
     }
+
+
 }
