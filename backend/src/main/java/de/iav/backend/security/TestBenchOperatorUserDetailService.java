9d54b9f91e7d21516015f9caa55ff87e554888bc
Mathias.Romankiewicz <Mathias.romankiewicz@gmail.com>
2023-09-21 10:57:17 +0200

    add TestBenchOperatorUserDetailService

---
+++ backend/src/main/java/de/iav/backend/security/TestBenchOperatorUserDetailService.java
@@ -0,0 +1,27 @@
+package de.iav.backend.security;
+
+import de.iav.backend.exceptions.TestBenchOperatorUsernameNotFoundExeption;
+import de.iav.backend.repository.TestBenchOperatorRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
+
+public class TestBenchOperatorUserDetailService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    private final TestBenchOperatorRepository testBenchOperatorRepository;
+
+    public TestBenchOperatorUserDetailService(TestBenchOperatorRepository testBenchOperatorRepository) {
+        this.testBenchOperatorRepository = testBenchOperatorRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return testBenchOperatorRepository.findTestBenchOperatorByUsername(username)
+                .map(foundUser -> new User(foundUser.username(), foundUser.password(), List.of()))
+                .orElseThrow(TestBenchOperatorUsernameNotFoundExeption::new);
+    }
+}
+
