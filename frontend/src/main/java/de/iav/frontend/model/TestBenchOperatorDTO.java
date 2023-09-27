b8d451d6af4f72aa6ceff1746c6241b6064d4b3a
Mathias.Romankiewicz <Mathias.romankiewicz@gmail.com>
2023-09-27 08:26:56 +0200

    add TestBenchOperatorDTO

---
+++ frontend/src/main/java/de/iav/frontend/model/TestBenchOperatorDTO.java
@@ -0,0 +1,10 @@
+package de.iav.frontend.model;
+
+public record TestBenchOperatorDTO(
+        String username,
+        String password,
+        String firstName,
+        String lastName,
+        String eMail
+) {
+}
