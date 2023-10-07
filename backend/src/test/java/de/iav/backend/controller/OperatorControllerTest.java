ef3dbec72baea9aa6c7b5c1e4ab4fb9dc832c431
Mathias.Romankiewicz <Mathias.romankiewicz@gmail.com>
2023-10-07 10:25:43 +0200

    add 10th test -> operator

--- backend/src/test/java/de/iav/backend/controller/OperatorControllerTest.java
+++ backend/src/test/java/de/iav/backend/controller/OperatorControllerTest.java
@@ -100,4 +100,40 @@ class OperatorControllerTest {
                 .andExpect(jsonPath("$.testBench.length()").value(0))
                 .andExpect(jsonPath("$.role").value("OPERATOR"));
     }
+
+    @Test
+    @DirtiesContext
+    @WithMockUser
+    void findOperatorByUsername_whenOperatorExists_thenReturnOperator() throws Exception {
+
+        Operator operator =new Operator(
+                "1",
+                "Jackass",
+                "Johnny",
+                "Knoxville",
+                "johnny.knoxville@jackass.com",
+                new ArrayList<>(),
+                UserRole.OPERATOR);
+
+        MvcResult response = mockMvc.perform(MockMvcRequestBuilders.post("/api/operators")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(operator)))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        Operator expectedOperator = objectMapper
+                .readValue(response
+                        .getResponse()
+                        .getContentAsString(), Operator.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/operators/"
+                        + expectedOperator.username()))
+                .andExpect(status().isAccepted())
+                .andExpect(jsonPath("$.username").value("Jackass"))
+                .andExpect(jsonPath("$.firstName").value("Johnny"))
+                .andExpect(jsonPath("$.lastName").value("Knoxville"))
+                .andExpect(jsonPath("$.email").value("johnny.knoxville@jackass.com"))
+                .andExpect(jsonPath("$.testBench.length()").value(0))
+                .andExpect(jsonPath("$.role").value("OPERATOR"));
+    }
 }
