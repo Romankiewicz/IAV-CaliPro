ff709c2b5333405eddcf17d1038eec4371acf81b
Mathias.Romankiewicz <Mathias.romankiewicz@gmail.com>
2023-10-11 10:59:09 +0200

    delete unused imports

--- backend/src/main/java/de/iav/backend/controller/OperatorController.java
+++ backend/src/main/java/de/iav/backend/controller/OperatorController.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/operators")
--- backend/src/main/java/de/iav/backend/controller/TestBenchController.java
+++ backend/src/main/java/de/iav/backend/controller/TestBenchController.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/testbenches")
