439e7cdc6f76f1185e2961e6e810d6063e11ee13
Mathias.Romankiewicz <Mathias.romankiewicz@gmail.com>
2023-10-12 09:02:26 +0200

    add MetrologistViewController variables

--- frontend/src/main/java/de/iav/frontend/controller/MetrologistViewController.java
+++ frontend/src/main/java/de/iav/frontend/controller/MetrologistViewController.java
@@ -1,6 +1,7 @@
 package de.iav.frontend.controller;
 
 import de.iav.frontend.model.Metrology;
+import de.iav.frontend.model.TestBench;
 import de.iav.frontend.service.SceneSwitchService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -15,6 +16,8 @@ public class MetrologistViewController {
     @FXML
     private ListView<Metrology> LV_METROLOGY;
     @FXML
+    private ListView<TestBench> LV_BENCH;
+    @FXML
     private Label LF_MESSAGE;
     @FXML
     private Button PB_HOME;
@@ -24,6 +27,7 @@ public class MetrologistViewController {
     private Button PB_DETAIL;
     @FXML
     private final SceneSwitchService sceneSwitchService = SceneSwitchService.getInstance();
+
 
     @FXML
     public void onClick_PB_HOME(ActionEvent event) throws IOException {
