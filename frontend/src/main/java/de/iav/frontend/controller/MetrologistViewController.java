a2eaf364d35d47e1d018cf9fef3a87edb81989ca
Mathias.Romankiewicz <Mathias.romankiewicz@gmail.com>
2023-10-12 08:21:31 +0200

    add DetailViews

--- frontend/src/main/java/de/iav/frontend/controller/MetrologistViewController.java
+++ frontend/src/main/java/de/iav/frontend/controller/MetrologistViewController.java
@@ -1,19 +1,32 @@
 package de.iav.frontend.controller;
 
+import de.iav.frontend.model.Metrology;
 import de.iav.frontend.service.SceneSwitchService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 import java.io.IOException;
 
 public class MetrologistViewController {
 
     @FXML
+    private ListView<Metrology> LV_METROLOGY;
+    @FXML
+    private Label LF_MESSAGE;
+    @FXML
+    private Button PB_HOME;
+    @FXML
+    private Button PB_ADD_METROLOGY;
+    @FXML
+    private Button PB_DETAIL;
+    @FXML
     private final SceneSwitchService sceneSwitchService = SceneSwitchService.getInstance();
 
     @FXML
     public void onClick_PB_HOME(ActionEvent event) throws IOException {
-        SceneSwitchService.getInstance().switchToStartView(event);
+        sceneSwitchService.switchToStartView(event);
     }
 }
