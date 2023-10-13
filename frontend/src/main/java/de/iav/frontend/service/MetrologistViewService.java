c9f7db37426b17ba366dbfaa268324690a7a80a6
Mathias.Romankiewicz <Mathias.romankiewicz@gmail.com>
2023-10-13 17:47:36 +0200

    add deleteMetrology method

--- frontend/src/main/java/de/iav/frontend/service/MetrologistViewService.java
+++ frontend/src/main/java/de/iav/frontend/service/MetrologistViewService.java
@@ -19,7 +19,7 @@ public class MetrologistViewService {
     private final AuthenticationService authenticationService = AuthenticationService.getInstance();
     private final HttpClient metrologistClient = HttpClient.newHttpClient();
     private final ObjectMapper objectMapper = new ObjectMapper();
-    private String IAVCALIPRO_URL_BACKEND = System.getenv("BACKEND_IAVCALIPRO_URI");
+    private final String IAVCALIPRO_URL_BACKEND = System.getenv("BACKEND_IAVCALIPRO_URI");
     private static final String JSON = "application/json";
 
     public MetrologistViewService() {
@@ -61,12 +61,46 @@ public class MetrologistViewService {
                 .join();
     }
 
+    public Metrology getMetrologyById(String metrologyId) {
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(IAVCALIPRO_URL_BACKEND + "metrology/" + metrologyId))
+                .header("Accept", JSON)
+                .header("Cookie", "JSESSEIONID=" +authenticationService.getSessionId())
+                .GET()
+                .build();
+
+        return metrologistClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenApply(this::mapToMetrology)
+                .join();
+    }
+
+    public void deleteMetrology(String metrologyId) {
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(IAVCALIPRO_URL_BACKEND + "metrology/" + metrologyId))
+                .header("Cookie", "JSESSIONID=" + authenticationService.getSessionId())
+                .DELETE()
+                .build();
+        metrologistClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .join();
+    }
+
 
 
     private List<Metrology> mapToMetrologyList(String responseBody) {
         try {
             return objectMapper.readValue(responseBody, new TypeReference<>() {
             });
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Metrology mapToMetrology(String responseBody) {
+        try {
+            return objectMapper.readValue(responseBody, Metrology.class);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
